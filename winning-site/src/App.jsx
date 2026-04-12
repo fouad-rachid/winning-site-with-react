505d7a65781027481537ea42273641291a86ed46
rachid.fouad <rachidfouad55@gmail.com>
2026-04-12 09:38:34 +0100

    starter-basic

---
+++ winning-site/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='border border-amber-500 text-5xl'>
+      <h1>hello there</h1>
+    </div>
+  )
+}
+
+export default App
