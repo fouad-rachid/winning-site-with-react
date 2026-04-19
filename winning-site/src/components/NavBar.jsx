bf3ffc66153e3f06b44a32ea1d115640c72e6892
rachid.fouad <rachidfouad55@gmail.com>
2026-04-19 09:29:43 +0100

    added navbar

---
+++ winning-site/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className='fixed top-0 left-0 z-50  md:p-9 p-3'>
+        <img src="/images/nav-logo.svg" alt="nav-logo" className='md:w-24 w20' />
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
