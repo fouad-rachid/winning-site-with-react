6624a13b3631c1235e9009ebda5d8a4df6f82915
rachid.fouad <rachidfouad55@gmail.com>
2026-04-20 10:30:11 +0100

    added-hero_section

--- winning-site/src/App.jsx
+++ winning-site/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import HeroSection from './sections/HeroSection'
 
 const App = () => {
   return (
     <div>
       <NavBar/>
+      <HeroSection/>
     </div>
   )
 }
